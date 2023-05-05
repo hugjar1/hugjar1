ad39e324ac29476d5c2102128893dc93fdae8e78
hugjar1 <115717218+hugjar1@users.noreply.github.com>
2023-05-05 11:56:39 +0200

    Test

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ let layer3 = document.getElementById('layer3')
 let layer4 = document.getElementById('layer4')
 let layer5 = document.getElementById('layer5')
 
-window.addEventListener('scroll', () => {
+function updatePositions() {
     let value = window.scrollY;
-
     text.style.marginTop = value * 2.5 + 'px';
     layer1.style.bottom = value * -0.90 + 'px';
     layer2.style.bottom = value * -1 + 'px';
     layer3.style.bottom = value * -0.75 + 'px';
     layer4.style.bottom = value * -0.5 + 'px';
     layer5.style.bottom = value * -0.35 + 'px';
+    requestAnimationFrame(updatePositions);
+}
 
-});
+requestAnimationFrame(updatePositions);
