6c491c1d7de85f946fa2cdd0ef4557b90795f60a
hugjar1 <hugojarvdalen@gmail.com>
2023-05-05 03:02:10 +0200

    Paralax basic layout 1

---
+++ script.js
@@ -0,0 +1,18 @@
+let text = document.getElementById('text')
+let layer1 = document.getElementById('layer1')
+let layer2 = document.getElementById('layer2')
+let layer3 = document.getElementById('layer3')
+let layer4 = document.getElementById('layer4')
+let layer5 = document.getElementById('layer5')
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+
+    text.style.marginTop = value * 2.5 + 'px';
+    layer1.style.bottom = value * -0.90 + 'px';
+    layer2.style.bottom = value * -1 + 'px';
+    layer3.style.bottom = value * -0.75 + 'px';
+    layer4.style.bottom = value * -0.5 + 'px';
+    layer5.style.bottom = value * -0.35 + 'px';
+
+});
